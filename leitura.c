274014f762d9fa2cbf4825ddbfbdb68cc679d88f
nataliapedroso <nataliapedroso@id.uff.br>
2017-10-03 15:06:55 -0300

    criação do arquivo para ler dados

---
+++ leitura.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main(int argc, char *argv[])
+{
+	int i, j;
+	float a, b, *x, *y;
+	FILE *arq;
+
+	arq=fopen(argv[1], "r");
+
+	i=0;
+	while(fscanf(arq, "%f %f", &a, &b)!=EOF)
+	{
+		x=malloc(sizeof(float*));
+		y=malloc(sizeof(float*));
+
+		x[i]=a;
+		y[i]=b;
+
+		i++;
+	}
+
+	printf("\n Dados lidos: \n");
+	for(j=0; j<i; j++)
+		 printf(" x=%f \t y=%f \n", x[j], y[j]);
+	printf("\n\n");
+}
